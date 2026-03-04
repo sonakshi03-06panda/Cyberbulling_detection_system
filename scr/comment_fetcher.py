ac4fb45a4e83cdbf072966d5940f40f8eeb71442
Sonakshi <sonakshi0306panda@outlook.com>
2026-03-04 19:05:31 +0530

    Update comment_fetcher.py

--- scr/comment_fetcher.py
+++ scr/comment_fetcher.py
@@ -127,7 +127,7 @@ if __name__ == "__main__":
     import os
     from dotenv import load_dotenv
     
-    load_dotenv()
+    load_dotenv(os.path.join(os.path.dirname(__file__), '..', '.env'))
     
     yt_key = os.getenv("YOUTUBE_API_KEY")
     fetcher = CommentFetcher(yt_key)
